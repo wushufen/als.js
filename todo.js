c20e3b469d5df17b1bbee7e1361e474514e479e0
wusfen <wusfun@foxmail.com>
2018-07-11 03:55:04 +0800

    .

--- todo.js
+++ todo.js
@@ -1,31 +1,31 @@
-als(function(type, url, data){
-​
-  var m = url.match('user/add')
-  if(m){
-​
-    als.table('user').insert(data)
-​
-    return {
-        code: 10000,
-        msg: 'success',
-        data: null
+// 添加ajax拦截规则
+als(function (type ,url, data) {
+
+    // 如果符合条件
+    var m = url.match(/api\/(.*)\/(.*)$/)
+    if (m) {
+
+        var table = m[1] // 表名
+        var action = m[2] // 操作 insert, update, save, delete, select
+
+        // action转换
+        if (action=='list') action = 'select'
+        // ...
+
+        // 本地数据库操作
+        var rs = als.table(table)[action](data)
+
+        // 返回结果作为ajax响应
+        return {
+            code: 10000,
+            msg: 'success',
+            data: rs
+        }
     }
-  }
-​
 })
 
-als(function(type, url, data){
-​
-  var m = url.match('user/detail')
-  if(m){
-​
-    var rs = als.table('user').select(data)
-​
-    return {
-        code: 10000,
-        msg: 'success',
-        data: data[0]
-    }
-  }
-​
-})
+
+// 开启ajax拦截
+if ('dev') {
+    als.open()
+}
